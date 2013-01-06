67da2dd0d92950c288014aa4886d2100f6178d9e
pragati <pragatirpatra@gmail.com>
2013-01-06 07:11:40 +0530

    worked on UI

--- Inventory/src/com/fnz/UI/MainWindow.java
+++ Inventory/src/com/fnz/UI/MainWindow.java
@@ -10,6 +10,7 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -46,9 +47,7 @@ public class MainWindow extends Application
 	private BorderPane border;
 	private Stage stage;
 	private Scene scene;
-	static DropShadow effect2 = new DropShadow();
-	static DropShadow effect1 = new DropShadow();
-	static DropShadow effect3 = new DropShadow();
+	
 	public boolean flag;
     /**
      * @param args the command line arguments
@@ -74,6 +73,7 @@ public class MainWindow extends Application
     		
         border = new BorderPane();
         border.setTop(upperPart());
+        border.setBottom(addBBox());
         
         scene = new Scene(border);
         scene.getStylesheets().add(
@@ -85,6 +85,7 @@ public class MainWindow extends Application
 	    stage.setHeight(Screen.getPrimary().getVisualBounds().getHeight());
 	    stage.setScene(this.scene);
         stage.setTitle("Inventory Management");
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("/com/fnz/styles/icon.png")));
         stage.show();
     	}
     	catch(Exception e)
@@ -92,7 +93,29 @@ public class MainWindow extends Application
     		e.printStackTrace();
     	}
     }
-   
+    private BorderPane addBBox(){
+    	javafx.scene.layout.
+        BorderPane border = new BorderPane();
+    	border.setPadding(new Insets(15, 12, 15, 12));
+       // hbox.setSpacing(10);   // Gap between nodes
+       // hbox.setStyle("-fx-background-color: #336699;");
+    	border.getStyleClass().add("lowerHBox");
+        Label lName=new Label("copyright © something");
+        border.setLeft(lName);
+        
+        
+        /*HBox hbox = new HBox();
+       
+        Button upload = new Button();
+        upload.setId("upload");
+        Image img1 = new Image("upload.png");
+        upload.setGraphic(new ImageView(img1));
+        upload.setContentDisplay(ContentDisplay.TOP);
+        hbox.getChildren().add(upload);
+        
+        border.setRight(hbox);*/
+        return border;
+    }
     
     /*Create Method <function name><return type><comments>
 	 * <Creator Name><Date Of Creation MM-dd-yyyy>
@@ -105,7 +128,8 @@ public class MainWindow extends Application
 	 * 
 	 * <Date> <Name> <Comments>
 	 * 
-	 */    
+	 */   
+   
     private BorderPane upperPart() 
     {
 
@@ -113,15 +137,130 @@ public class MainWindow extends Application
     	
     	TabPane tabPane = new TabPane();
     	tabPane.setId(("MyTabPane"));
-       
+    	
+    	
         //Create Tabs
         Tab tabA = new Tab();
-        tabA.setText("Company Details");
+        tabA.setText("Add/View Items ");
         
-        BorderPane borderPane1 = new BorderPane();
-        borderPane1.setPadding(new Insets(5,5,5,5));
+        final BorderPane borderPane1 = new BorderPane();
+        borderPane1.setMinWidth(Screen.getPrimary().getVisualBounds().getWidth());
+        borderPane1.setMinHeight(Screen.getPrimary().getVisualBounds().getHeight());
+        borderPane1.setPadding(new Insets(15,0,0,20));
         try {
-			borderPane1.setCenter(new Label("Window1"));
+        	
+        	borderPane1.setId("borderxx");
+        	
+        	VBox typesOfDrink = new VBox();
+        	ToggleGroup groupDrink=new ToggleGroup();
+        	
+        	ToggleButton tbWine= new ToggleButton("Wine");
+        	tbWine.setToggleGroup(groupDrink);
+        	tbWine.setId("drinkName");
+        	tbWine.setMaxSize(250,250);
+        	
+        	tbWine.setOnAction(new EventHandler<ActionEvent>() {
+     			
+     			@Override
+     			public void handle(ActionEvent e) 
+     			{
+     				borderPane1.setStyle("-fx-background-image: url('com/fnz/styles/wine.jpeg');");
+     				
+     				
+     			}
+     		});
+        	
+        	ToggleButton tbVodka= new ToggleButton("Vodka");
+        	tbVodka.setToggleGroup(groupDrink);
+        	tbVodka.setId("drinkName");
+        	tbVodka.setMaxSize(250,250);
+        	tbVodka.setOnAction(new EventHandler<ActionEvent>() {
+     			
+     			@Override
+     			public void handle(ActionEvent e) 
+     			{
+     				borderPane1.setStyle("-fx-background-image: url('com/fnz/styles/vodka.jpg');");
+     				
+     				
+     			}
+     		});
+        	
+        	ToggleButton tbBeer= new ToggleButton("Beer");
+        	tbBeer.setToggleGroup(groupDrink);
+        	tbBeer.setId("drinkName");
+        	tbBeer.setMaxSize(250,250);
+        	tbBeer.setOnAction(new EventHandler<ActionEvent>() {
+     			
+     			@Override
+     			public void handle(ActionEvent e) 
+     			{
+     				borderPane1.setStyle("-fx-background-image: url('com/fnz/styles/beer2.jpg');");
+     				
+     				
+     			}
+     		});
+
+        	ToggleButton tbWisky= new ToggleButton("Whisky");
+        	tbWisky.setToggleGroup(groupDrink);
+        	tbWisky.setId("drinkName");
+        	tbWisky.setMaxSize(250,250);
+        	tbWisky.setOnAction(new EventHandler<ActionEvent>() {
+     			
+     			@Override
+     			public void handle(ActionEvent e) 
+     			{
+     				borderPane1.setStyle("-fx-background-image: url('com/fnz/styles/whisky.jpg');");
+     				
+     				
+     			}
+     		});
+        	
+        	ToggleButton tbRum= new ToggleButton("Rum");
+        	tbRum.setToggleGroup(groupDrink);
+        	tbRum.setId("drinkName");
+        	tbRum.setMaxSize(250,250);
+        	tbRum.setOnAction(new EventHandler<ActionEvent>() {
+     			
+     			@Override
+     			public void handle(ActionEvent e) 
+     			{
+     				borderPane1.setStyle("-fx-background-image: url('com/fnz/styles/rum2.jpg');");
+     				
+     				
+     			}
+     		});
+        	
+        	ToggleButton tbScotch= new ToggleButton("Scotch");
+        	tbScotch.setToggleGroup(groupDrink);
+        	tbScotch.setId("drinkName");
+        	tbScotch.setMaxSize(250,250);
+        	tbScotch.setOnAction(new EventHandler<ActionEvent>() {
+     			
+     			@Override
+     			public void handle(ActionEvent e) 
+     			{
+     				borderPane1.setStyle("-fx-background-image: url('com/fnz/styles/Scotch.jpg');");
+     				
+     				
+     			}
+     		});
+        	ToggleButton tbOther= new ToggleButton("Other");
+        	tbOther.setToggleGroup(groupDrink);
+        	tbOther.setId("drinkName");
+        	tbOther.setMaxSize(250,250);
+        	tbOther.setOnAction(new EventHandler<ActionEvent>() {
+     			
+     			@Override
+     			public void handle(ActionEvent e) 
+     			{
+     				borderPane1.setStyle("-fx-background-image: url('com/fnz/styles/othertype.jpg');");
+     				
+     				
+     			}
+     		});
+            typesOfDrink.getChildren().addAll(tbWine,tbVodka,tbBeer,tbWisky,tbRum,tbScotch,tbOther);
+           
+			borderPane1.setLeft(typesOfDrink);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -130,7 +269,7 @@ public class MainWindow extends Application
         tabPane.getTabs().add(tabA);
        
         Tab tabB = new Tab();
-        tabB.setText("Tax Details");
+        tabB.setText("Delete/Update Items");
         BorderPane borderPane2 = new BorderPane();
         borderPane2.setPadding(new Insets(5,5,5,5));
         try {
