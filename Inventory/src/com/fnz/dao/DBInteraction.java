ad74894be5e3a39c93ae29bbc3863453b6e42069
friends-repository <abhinay_agarwal@live.com>
2013-02-03 23:16:38 +0530

    aa

--- Inventory/src/com/fnz/dao/DBInteraction.java
+++ Inventory/src/com/fnz/dao/DBInteraction.java
@@ -5,9 +5,14 @@ package com.fnz.dao;
 import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Properties;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 import org.sqlite.SQLiteConfig;
 
@@ -32,7 +37,9 @@ public class DBInteraction
 		
 		
 		//create Folder to store DB
-		File dir=new File("/C:/"+"/Inventory");
+		//File dir=new File("/C:/"+"/Inventory");
+		
+		File dir=new File("Data");
 		if(!dir.exists())
 		{
 			dir.mkdir();
@@ -58,6 +65,14 @@ public class DBInteraction
 			stmt.executeUpdate( SQLConstants.CREATE_INCOMING_STOCK );
 			stmt.executeUpdate(SQLConstants.CREATE_INCOMING_STOCK_DETAILS);
 			stmt.executeUpdate(SQLConstants.CREATE_OUTGOING_STOCK_DETAILS);
+			
+			ObservableList<String> newCategoryList =FXCollections.observableArrayList();
+	    	newCategoryList.addAll(CommonConstants.CATEGORY_PREMIUM_WHISKY,CommonConstants.CATEGORY_REGULAR_WHISKY,CommonConstants.CATEGORY_PREMIUM_VODKA,CommonConstants.CATEGORY_REGULAR_VODKA,
+	    			CommonConstants.CATEGORY_BRANDY,CommonConstants.CATEGORY_GIN,CommonConstants.CATEGORY_PREMIUM_RUM,
+	    			CommonConstants.CATEGORY_REGULAR_RUM,CommonConstants.CATEGORY_BEER,CommonConstants.CATEGORY_WINE,
+	    			CommonConstants.CATEGORY_PREMIUM_SCOTCH,CommonConstants.CATEGORY_REGULAR_SCOTCH,CommonConstants.CATEGORY_BEVRAGES);
+	    	
+	        addCategory(newCategoryList);
 		}
 		catch (Exception e) 
 		{
@@ -74,6 +89,95 @@ public class DBInteraction
 				stmt.close();
 			}
 		
+		}
+	}
+	
+	public void addCategory(ObservableList<String> categoryList) throws Exception 
+	{
+		SQLiteConfig config = null;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		PreparedStatement pstmt1 = null;
+		ResultSet resultSet = null;
+		Integer latestRow = 0;
+		String query="";
+		
+		String newCategoryId = CommonConstants.CATEGORY_ID;
+		
+		Class.forName(CommonConstants.DRIVERNAME);
+		
+		String sDbUrl = CommonConstants.sJdbc + ":" + CommonConstants.DB_LOCATION + CommonConstants.sTempDb;
+		
+		try 
+		{
+			config = new SQLiteConfig();
+			config.enforceForeignKeys(true);
+			conn = DriverManager.getConnection(sDbUrl, config.toProperties());
+			
+			
+			pstmt1 = conn.prepareStatement(SQLConstants.FETCH_LATEST_CATEGORY);
+			
+			resultSet = pstmt1.executeQuery();
+			
+			resultSet.next();
+			
+			latestRow = resultSet.getInt(1)+1;
+			
+			
+			
+			
+			for(int i = 0;i<categoryList.size();i++)
+			{
+				newCategoryId = CommonConstants.CATEGORY_ID;
+				if(latestRow <10)
+				{
+					newCategoryId = newCategoryId + "00" + latestRow.toString();
+				}
+				else if(latestRow >=10 && latestRow <100)
+				{
+					newCategoryId = newCategoryId + "0" + latestRow.toString();
+				}
+				else
+				{
+					newCategoryId = newCategoryId + latestRow.toString();
+				} 
+				latestRow++;
+				
+				query = query + " SELECT '"  + newCategoryId + "'"+CommonConstants.COMMA +"'"+ categoryList.get(i) + "' UNION ALL";
+				
+			}
+			
+			query = query.substring(0, query.length()-9);
+			query = query + ";";
+			
+			pstmt = conn.prepareStatement(SQLConstants.INSERT_CATEGORY_1+query);
+			
+			
+			pstmt.execute();
+			
+		}
+		catch (Exception e) 
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			if(conn !=null )
+			{
+				conn.close();
+			}
+			if(pstmt != null )
+			{
+				pstmt.close();
+			}
+			if(pstmt1 != null )
+			{
+				pstmt1.close();
+			}
+			if(resultSet != null)
+			{
+				resultSet.close();
+			}
 		}
 	}
 }
