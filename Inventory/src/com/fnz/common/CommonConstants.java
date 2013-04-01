e74cf75ca555b3ad14d010ef04d31c2c39ce56da
friends-repository <abhinay_agarwal@live.com>
2013-04-02 01:23:30 +0530

    Fixed few error

--- Inventory/src/com/fnz/common/CommonConstants.java
+++ Inventory/src/com/fnz/common/CommonConstants.java
@@ -79,5 +79,5 @@ public class CommonConstants
 	public static final String QTY_LOW = " Low in Stock";
 	public static final String DATE_COMPARE = "Start date can't be greater than End date";
 
-	public static final String PURCHASE_INVOICE_ERROR = "Invoice Id already exists. Incase of incorrect entry, please delete from history";
+	public static final String PURCHASE_INVOICE_ERROR = "InvoiceId & Date already exists. Incase of incorrect entry, please delete from history";
 }
